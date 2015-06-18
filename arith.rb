6bbf65a75c5b63b5aa0a61cbd905c2605573bfe5
karthigashree <ashwinibace@gmail.com>
2015-06-18 12:09:05 +0530

    Update arith.rb

--- arith.rb
+++ arith.rb
@@ -1,1 +1,89 @@
 
+
+prompt = "? " 
+puts "Enter 1 or 2 or 3 or 4 or 5  or 6 ." 
+print prompt
+input = gets.chomp.to_i
+
+if input == 1 
+   	 def addtwo(a, b) 
+      	 a + b; 
+     	end
+ 
+ 	 puts "Please enter number 1 : ";
+
+  	x = gets;
+ 	 puts "Please enter number 2 : ";
+ 	 y = gets;
+	 puts "Answer : " , (x.to_i + y.to_i), "\n";   
+
+   
+elsif input == 2   
+  	def addtwo(a, b) 
+      	 a - b; 
+  	end
+ 
+ 	puts"Please enter number 1 : ";
+
+ 	 x = gets
+ 	puts "Please enter number 2 : ";
+ 	 y = gets;
+
+ 	 puts "Answer : " , (x.to_i - y.to_i), "\n";
+end
+if  input == 3
+
+ 	def addtwo(a, b) 
+      	 a * b; 
+  	end
+ 
+ 	puts"Please enter number 1 : ";
+
+ 	 x = gets
+ 	puts "Please enter number 2 : ";
+ 	 y = gets;
+	
+  	puts "Answer : " , (x.to_i * y.to_i), "\n";
+
+
+elsif input == 4 
+   	 def addtwo(a, b) 
+      	 a / b; 
+     	end
+ 
+ 	 puts "Please enter number 1 : ";
+
+  	x = gets;
+ 	 puts "Please enter number 2 : ";
+ 	 y = gets;
+	 puts "Answer : " , (x.to_i / y.to_i), "\n";   
+end
+   
+if input == 5 
+  	def addtwo(a, b) 
+      	 a % b; 
+  	end
+ 
+ 	puts"Please enter number 1 : ";
+
+ 	 x = gets
+ 	puts "Please enter number 2 : ";
+ 	 y = gets;
+
+ 	 puts "Answer : " , (x.to_i % y.to_i), "\n";
+
+elsif  input == 6
+
+ 	def addtwo(a, b) 
+      	 a ** b; 
+  	end
+ 
+ 	puts"Please enter number 1 : ";
+
+ 	 x = gets
+ 	puts "Please enter number 2 : ";
+ 	 y = gets;
+	
+  	puts "Answer : " , (x.to_i ** y.to_i), "\n"; 
+end
+
